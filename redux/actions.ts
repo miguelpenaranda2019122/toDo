c79e6cdd8b09302d7ef1e43ad26b785897ffe071
miguelpenaranda2019122 <2019122@student.uma.pt>
2024-02-16 16:09:33 +0000

    optimize actions to the modern way

--- redux/actions.ts
+++ redux/actions.ts
@@ -1,33 +1,33 @@
 import { Work } from '../services/worksListInfo';
-export const SET_WORKLIST_INFO = 'SET_WORKLIST_INFO';
-export const SET_TEXT_VALUE = 'SET_TEXT_VALUE';
-export const SET_TEXTAREA_VALUE = 'SET_TEXTAREA_VALUE';
-export const SET_MODAL_VISIBLE = 'SET_MODAL_VISIBLE';
+export const SET_WORKLIST_INFO = 'todo/worklistUpdated';
+export const SET_TEXT_VALUE = 'todo/textUpdated';
+export const SET_TEXTAREA_VALUE = 'todo/textAreaUpdated';
+export const SET_MODAL_VISIBLE = 'todo/modalStatusUpdated';
 
-export const setWorkList = (workList: Work[]) => (dispatch: (obj: Object) => void) => {
-    dispatch({
+export const setWorkList = (worksList: Work[]) => {
+    return {
         type: SET_WORKLIST_INFO,
-        payload: workList
-    });
+        payload: worksList
+    };
 };
 
-export const setText = (text: string) => (dispatch: (obj: Object) => void) => {
-    dispatch({
-        type: SET_TEXT_VALUE,
+export const setText = (text: string) => {
+    return {
+        type: SET_WORKLIST_INFO,
         payload: text
-    });
+    };
 };
 
-export const setTextArea = (textArea: string) => (dispatch: (obj: Object) => void) => {
-    dispatch({
-        type: SET_TEXTAREA_VALUE,
+export const setTextArea = (textArea: string) => {
+    return {
+        type: SET_WORKLIST_INFO,
         payload: textArea
-    });
+    };
 };
 
-export const setModalVisible = (modalVisible: boolean) => (dispatch: (obj: Object) => void) => {
-    dispatch({
+export const setModalVisible = (modalVisible: boolean) => {
+    return{
         type: SET_MODAL_VISIBLE,
         payload: modalVisible
-    });
+    };
 };
